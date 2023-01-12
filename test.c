1765924fabde2b36a94be05f589cb172843afcd5
24stage <117885178+24stage@users.noreply.github.com>
2023-01-12 18:13:54 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int num,sum=0;
+    scanf("%d",&num);
+    while(num)
+    {
+        sum=sum*10+num%10;
+        num/=10;
+    }
+    printf("%d",sum);
+
+
+    system("pause");
+    return 0;
+}
